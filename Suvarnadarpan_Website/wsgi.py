c52c547abd566f142618247b4dd48a83f106f1b4
Harsh8naidu <harsh8naidu@gmail.com>
2022-03-27 12:47:48 +0530

    making changes to this file

---
+++ Suvarnadarpan_Website/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Suvarnadarpan_Website project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Suvarnadarpan_Website.settings')
+
+application = get_wsgi_application()
